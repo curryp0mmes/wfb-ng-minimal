ddde849fe140e384dfbbfd95e6f137189f30b3d7
Eleocraft <eliseo.cailloux@gmx.de>
2024-11-05 17:23:53 +0100

    new libcam pipeline

--- air/libcamera.sh
+++ air/libcamera.sh
@@ -3,8 +3,8 @@
 gst-launch-1.0 libcamerasrc ! \
     video/x-raw,width=1280,height=720,framerate=60/1 ! \
     videoconvert ! \
-    v4l2h264enc bitrate=6000000 ! \
-    video/x-h264,profile=high ! \
+    v4l2h264enc name=rpi_v4l2_encoder extra-controls="controls,video_bitrate=6000000" ! \
+    "video/x-h264,level=(string)4,profile=constrained-baseline" ! \
     h264parse ! \
     rtph264pay config-interval=1 pt=96 ! \
-    udpsink host=localhost port=5602
+    udpsink host=localhost port=5600
